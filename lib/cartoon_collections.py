6d144f924543ad50a9acf29e1ae697abdd7f20aa
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-08 09:45:09 +0300

    cartoon_lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,19 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i, name in enumerate(names, start=1):
+        print(f"{i}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planteers):
+    planteers = [planteer.capitalize() + "!" for planteer in planteers]
+    return planteers
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    return any(len(call) > 4 for call in calls)
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    cheese = ["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in cheese:
+            return food
+        
+    return None
+    
